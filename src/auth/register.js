102f04e6a4daf04d6abb29485d0b51a1d21f63c2
Faris Luthfan R <lrfaris24@gmail.com>
2019-02-21 18:43:31 +0700

    update: - register layout

--- src/auth/register.js
+++ src/auth/register.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
-import { Text, Container, Header, Content, Button } from 'native-base'
-import { View } from 'react-native'
+import {
+  Text,
+  Container,
+  Content,
+  Button,
+  Form,
+  Item,
+  Label,
+  Input,
+  Footer,
+  FooterTab,
+  Body,
+  ListItem
+} from 'native-base'
+import { View, CheckBox, StyleSheet } from 'react-native'
 
 export default class Register extends Component {
   static navigationOptions = {
@@ -10,10 +23,52 @@ export default class Register extends Component {
   render() {
     return (
       <Container>
-        <Content>
-          <Text>Register</Text>
+        <Content padder>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Email</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Full Name</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Password</Label>
+              <Input />
+            </Item>
+            <Item floatingLabel>
+              <Label>Password Confirmation</Label>
+              <Input />
+            </Item>
+          </Form>
+          <View style={style.checkboxLayout}>
+            <CheckBox />
+            <Text style={{ paddingTop: 5 }}>
+              I agree with all term and conditions.
+            </Text>
+          </View>
         </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Sign Up</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
       </Container>
     )
   }
 }
+
+const style = StyleSheet.create({
+  checkboxLayout: {
+    flex: 1,
+    flexDirection: 'row',
+    marginVertical: 24
+  }
+})
